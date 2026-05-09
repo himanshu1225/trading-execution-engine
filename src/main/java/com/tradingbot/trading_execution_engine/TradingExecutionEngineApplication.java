998183dbdeadc6ddcdf7dfbd190027f686ba84b0
Himanshu Bhati <your-email@example.com>
2026-05-09 23:26:19 +0530

    feat: add initial spring boot setup

---
+++ src/main/java/com/tradingbot/trading_execution_engine/TradingExecutionEngineApplication.java
@@ -0,0 +1,13 @@
+package com.tradingbot.trading_execution_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingExecutionEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingExecutionEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradingbot/trading_execution_engine/TradingExecutionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradingbot.trading_execution_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingExecutionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
